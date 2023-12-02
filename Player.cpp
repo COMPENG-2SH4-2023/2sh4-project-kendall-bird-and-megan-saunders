fc6343a6ec0c01f2557851669292ef098548bec3
unknown <birdk7@mcmaster.ca>
2023-12-01 19:24:18 -0500

    try 1

--- Player.cpp
+++ Player.cpp
@@ -7,26 +7,76 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
+    playerPos.setObjPos(mainGameMechsRef->getBoardSizeX() / 2, mainGameMechsRef->getBoardSizeY() / 2, '@');
+
+    // no heap member yet - never used new keyword
 }
 
 
 Player::~Player()
 {
     // delete any heap members here
+    // leave empty for iteration 1
 }
 
 void Player::getPlayerPos(objPos &returnPos)
 {
+    returnPos.setObjPos(playerPos.x, playerPos.y, playerPos.symbol);
     // return the reference to the playerPos arrray list
 }
 
 void Player::updatePlayerDir()
 {
-    // PPA3 input processing logic        
+    // PPA3 input processing logic  
+
+    // where do i get the input? how to check for input?
+    // hint 1) not by calling MacUILib_getChar()
+    // hint 2) coordinate w team member designing GameMechanism class
+    // there will be a method in GameMechanism class that 
+    // collectively checks input and store the most recent input. You just need to figure out how to get to it
+    // how?? it lies within the GameMechs* inside your private member
+
+    char input = mainGameMechsRef->getInput();
+
+    switch(input)
+    {
+        case 'w':
+            if(myDir != UP && myDir != DOWN)
+            {
+                myDir = UP;
+            }
+            break;
+        
+        case 'a':
+            if(myDir != LEFT && myDir != RIGHT)
+            {
+                myDir = LEFT;
+            }
+            break;
+        
+        case 's':
+            if(myDir != UP && myDir != DOWN)
+            {
+                myDir = DOWN;
+            }
+            break;
+        
+        case 'd':
+            if(myDir != LEFT && myDir != RIGHT)
+            {
+                myDir = RIGHT;
+            }
+            break;
+        
+        default:
+            break;
+    }
+       
 }
 
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
+
 }
 
--- Project.cpp
+++ Project.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "GameMechs.h"
+#include "Player.h"
 
 
 using namespace std;
 
 #define DELAY_CONST 100000
 
-bool exitFlag;
+GameMechs* myGM;
+Player* myPlayer;
+
+// objPos myPos; (he wrote then deleted this - may need to uncomment)
+
+//bool exitFlag; (he deleted this in vid)
 
 void Initialize(void);
 void GetInput(void);
@@ -23,7 +30,7 @@ int main(void)
 
     Initialize();
 
-    while(exitFlag == false)  
+    while(myGM->getExitFlagStatus() == false)  
     {
         GetInput();
         RunLogic();
@@ -40,8 +47,9 @@ void Initialize(void)
 {
     MacUILib_init();
     MacUILib_clearScreen();
-
-    exitFlag = false;
+    
+    myGM = new GameMechs(26, 13); // make board size 26x13
+    myPlayer = new Player(myGM);
 }
 
 void GetInput(void)
@@ -51,12 +59,21 @@ void GetInput(void)
 
 void RunLogic(void)
 {
-    
+    myPlayer->updatePlayerDir();
 }
 
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();
+
+    objPos tempPos;
+    myPlayer->getPlayerPos(tempPos); //get player pos
+
+    MacUILib_printf("Board Size: %dx%d, Player Pos: <%d, %d> + %c\n", 
+        myGM->getBoardSizeX(), 
+        myGM->getBoardSizeY(),
+        tempPos.x, tempPos.y, tempPos.symbol); 
+    // Bcus we are using the async input in MacUILib, we have to use MacUILib_printf() instead of cout
 
 }
 
